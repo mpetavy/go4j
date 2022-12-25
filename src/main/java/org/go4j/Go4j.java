307c288135b314eb5da10390088c6f387b0840a6
mpetavy <mpetavy>
2022-12-25 18:48:44 +0100

    Some method refactoring

--- src/main/java/org/go4j/Go4j.java
+++ src/main/java/org/go4j/Go4j.java
@@ -6,15 +6,23 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Go4j {
-    private static ExecutorService executorService = Executors.newCachedThreadPool();
+    private static final ExecutorService executorService = Executors.newCachedThreadPool();
 
-    public static Future<?> run(Runnable task) {
+    public static Future<?> go(Runnable task) {
         return executorService.submit(task);
     }
 
-    public static Future<?> run(Callable<?> task) {
+    public static Future<?> go(Callable<?> task) {
         return executorService.submit(task);
     }
+
+    public static Thread async(Runnable task) {
+        Thread thread = new Thread(task);
+        thread.start();
+
+        return thread;
+    }
+
 
     public static void sleep(long milliseconds) {
         try {
@@ -27,7 +35,7 @@ public class Go4j {
     public static Channel<Boolean> after(long milliseconds) {
         Channel<Boolean> ch = new Channel<>();
 
-        run(() -> {
+        go(() -> {
             sleep(milliseconds);
             ch.write(true);
         });
